4f8cb0a39240273ddb04e157952d3569c0571cdb
alexisemploymentagency-astro <alexis.employmentagency@gmail.com>
2026-02-25 00:02:21 +0800

    Update Live Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
-
 import netlify from '@astrojs/netlify';
+
+// The server checks if it's currently running on Netlify's live servers
+const isNetlifyLive = process.env.NETLIFY === 'true';
 
 export default defineConfig({
   output: 'server', // THIS IS THE MAGIC SWITCH!
-  adapter: netlify(),
+  
+  // THE SMART ADAPTER: 
+  // If true -> use Netlify. If false (StackBlitz) -> use Node!
+  adapter: isNetlifyLive ? netlify() : node({ mode: 'standalone' }),
+  
   integrations: [tailwind()],
 });
