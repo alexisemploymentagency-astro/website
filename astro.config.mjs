17159d9e22563879a111fcf9157803399d90652c
alexisemploymentagency-astro <alexis.employmentagency@gmail.com>
2026-02-24 13:14:54 +0800

    Fix netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server', // THIS IS THE MAGIC SWITCH!
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
   integrations: [tailwind()],
 });
